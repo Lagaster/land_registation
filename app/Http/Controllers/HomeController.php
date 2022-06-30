83652e6a2a9dcfee469fab5bbe41dadcd003c20c
abrahamkivosh <abrahamkivosh@gmail.com>
2022-06-30 19:54:25 +0300

    modf dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Land;
+use App\Models\LandRate;
+use App\Models\LandUser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +27,18 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        $user = Auth::user();
+        $mylandscount = LandUser::with('land')->whereBelongsTo($user,'owner')
+        ->where('is_owner',true)
+        ->count();
+        $landscount = Land::count();
+        $landratescount = LandRate::count();
+        $mylands = LandUser::with('land')->whereBelongsTo($user,'owner')
+        ->where('is_owner',true)
+        ->get();
+
+        $landRates=LandRate::all();
+
+        return view('admin.index',compact('mylandscount','landscount','landratescount','mylands','landRates'));
     }
 }
