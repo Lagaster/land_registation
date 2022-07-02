120bb15cac5bcad64a34073280940f489513c820
abrahamkivosh <abrahamkivosh@gmail.com>
2022-07-02 20:09:08 +0300

    d

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,18 +16,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        $this->call(
-            [
-                LandSeeder::class,
-                LandRateSeeder::class,
-                LandUserSeeder::class,
-                PaymentSeeder::class,
-                StampDutySeeder::class,
-                ValuationReportSeeder::class,
-                LandUserSeeder::class
-            ]
-        );
+        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => "Land registrar",
+            'email' => "registrar@gmail.com",
+            'email_verified_at' => now(),
+            'password' => Hash::make("password"), // password
+            'dob'=>now(),
+            'image'=>"image",
+            'national_id'=>"12345678",
+            'id_image'=>"himage",
+            'phone'=>"0712345678",
+            'address'=>"Kenya,Nairobi",
+            'role'=>"registrar",
+            'kra_pin'=>"A2345678",
+            'gender'=>"female"
+        ]);
+
+        // $this->call(
+        //     [
+        //         LandSeeder::class,
+        //         LandRateSeeder::class,
+        //         LandUserSeeder::class,
+        //         PaymentSeeder::class,
+        //         StampDutySeeder::class,
+        //         ValuationReportSeeder::class,
+        //         LandUserSeeder::class
+        //     ]
+        // );
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
